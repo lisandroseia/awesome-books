6dd141daa541a55f30d6d811792cb748d27b58a1
lisandroseia <lisandroseia11@gmail.com>
2022-01-03 16:24:57 -0300

    Select nodes with js

---
+++ script.js
@@ -0,0 +1,14 @@
+let colection = [];
+
+const inputTitle = document.getElementById('title');
+const inputAuthor = document.getElementById('author');
+const submitBtn = document.querySelector('.add-btn');
+
+const book =  {
+    title: '',
+    author: ''
+}
+
+
+
+// console.log(inputTitle, inputAuthor, submitBtn)
