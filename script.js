28a3ebdfeba7b64f73269a6f5c504802b6ac0a66
Zeeshan Haider <zhadier39@gmail.com>
2022-01-04 03:14:57 +0500

    Fix eslint errors

--- script.js
+++ script.js
@@ -1,56 +1,55 @@
 let collection = [];
+let arr = [];
 
-const inputTitle = document.getElementById("title");
-const inputAuthor = document.getElementById("author");
-const submitBtn = document.querySelector(".add-btn");
+const inputTitle = document.getElementById('title');
+const inputAuthor = document.getElementById('author');
+const submitBtn = document.querySelector('.add-btn');
 const bookSection = document.querySelector('.books');
 
-
 const book = {
-    title: "",
-    author: "",
+  title: '',
+  author: '',
 };
 
-submitBtn.addEventListener("click", () => {
-
-    if (inputTitle.value !== '' && inputAuthor.value !== '') {
-        const newBook = Object.create(book);
-        newBook.title = inputTitle.value;
-        newBook.author = inputAuthor.value;
-        collection.push(newBook);
-        populateStorage();
-        addBook(newBook);
-    }
-
-
-});
-
-let arr = []
+function populateStorage() {
+  localStorage.setItem('Books', JSON.stringify({
+    coll: collection,
+  }));
+}
 
 function addBook(book) {
-    const div = document.createElement('div');
-    div.className = `book-wraper`;
-    div.setAttribute('data-value', `${book.title}-${book.author }`);
-    div.innerHTML = `<h3>${book.title}</h3>
+  const div = document.createElement('div');
+  div.className = 'book-wraper';
+  div.setAttribute('data-value', `${book.title}-${book.author}`);
+  div.innerHTML = `<h3>${book.title}</h3>
                   <h3>${book.author}</h3>
                   <button type="button" class ="remove-button">Remove</button>`;
-    bookSection.appendChild(div);
-    const removeBtn = document.querySelectorAll('.remove-button');
-    removeBtn[removeBtn.length - 1].addEventListener('click', (evt) => {
-        document.querySelector('.books').removeChild(evt.target.parentNode);
-        arr = evt.target.parentNode.getAttribute('data-value').split('-');
-        collection = collection.filter(item => item.title !== arr[0] && item.author !== arr[1]);
-        populateStorage();
-    });
+  bookSection.appendChild(div);
+  const removeBtn = document.querySelectorAll('.remove-button');
+  removeBtn[removeBtn.length - 1].addEventListener('click', (evt) => {
+    document.querySelector('.books').removeChild(evt.target.parentNode);
+    arr = evt.target.parentNode.getAttribute('data-value').split('-');
+    collection = collection.filter((item) => item.title !== arr[0] && item.author !== arr[1]);
+    populateStorage();
+  });
 }
 
-function populateStorage() {
-    localStorage.setItem('Books', JSON.stringify({
-        coll: collection,
-    }));
-}
+submitBtn.addEventListener('click', () => {
+  if (inputTitle.value !== '' && inputAuthor.value !== '') {
+    const newBook = Object.create(book);
+    newBook.title = inputTitle.value;
+    newBook.author = inputAuthor.value;
+    collection.push(newBook);
+    populateStorage();
+    addBook(newBook);
+  }
+});
 
 const localBooks = JSON.parse(localStorage.getItem('Books'));
 localBooks.coll.forEach((element) => {
-    addBook(element);
+  addBook(element);
 });
+
+window.onload = () => {
+  collection = localBooks.coll;
+};
