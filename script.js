cbf05b821451702235f2ebec55c007acff89ec7f
Zeeshan Haider <zhadier39@gmail.com>
2022-01-04 00:38:50 +0500

    create function to save object into array

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-let colection = [];
+let collection = [];
 
-const inputTitle = document.getElementById('title');
-const inputAuthor = document.getElementById('author');
-const submitBtn = document.querySelector('.add-btn');
+const inputTitle = document.getElementById("title");
+const inputAuthor = document.getElementById("author");
+const submitBtn = document.querySelector(".add-btn");
 
-const book =  {
-    title: '',
-    author: ''
-}
+const book = {
+  title: "",
+  author: "",
+};
 
-
+submitBtn.addEventListener("click", () => {
+  const newBook = Object.create(book);
+  newBook.title = inputTitle.value;
+  newBook.author = inputAuthor.value;
+  console.log(newBook);
+  collection.push(newBook);
+});
 
 // console.log(inputTitle, inputAuthor, submitBtn)
