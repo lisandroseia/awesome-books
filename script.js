a42c6e2fdb4b9fdcecb1ba39c3ad34623cd4b285
Zeeshan Haider <zhadier39@gmail.com>
2022-01-05 00:43:00 +0500

    Display books on add

--- script.js
+++ script.js
@@ -8,27 +8,36 @@ class collection {
     this.books = books;
   }
 
-  add(data){
-     this.books.push(data);
+  add(data) {
+    this.books.push(data);
+    this.display(data);
   }
 
-  remove(data){
-    
+  remove(data) {
+
   }
 
+  display(data) {
+  const div = document.createElement('div');
+  div.className = 'book-wraper';
+  div.innerHTML = `<h3>${data.title}</h3>
+                  <h3>${data.author}</h3>
+                  <button type="button" class ="remove-button">Remove</button>`;
+  bookSection.appendChild(div);
+}
 }
 
 class book {
- constructor(title, author){
-   this.title = title;
-   this.author = author;
- }
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
 
 const coll = new collection;
 
-submitBtn.addEventListener('click', () =>{
-coll.add(  new book(inputTitle.value, inputAuthor.value));
+submitBtn.addEventListener('click', () => {
+  coll.add(new book(inputTitle.value, inputAuthor.value));
 })
 
 
