cc5e58d174020b5ce78fecc5fe201a42f751f623
lisandroseia <lisandroseia11@gmail.com>
2022-01-04 17:11:57 -0300

    Add removal functionality

--- script.js
+++ script.js
@@ -9,12 +9,20 @@ class collection {
   }
 
   add(data) {
+    if(this.books.filter(item => item.author == data.author && item.title == data.title).length > 0){
+      return;
+    }
     this.books.push(data);
     this.display(data);
+    this.remove();
   }
 
-  remove(data) {
-
+  remove() {
+    const removeBtns = document.querySelectorAll('.remove-button');
+    removeBtns[removeBtns.length - 1].addEventListener('click', (evt) => {
+      bookSection.removeChild(evt.target.parentNode);
+      this.removeFromColl(evt.target);
+    })
   }
 
   display(data) {
@@ -22,9 +30,15 @@ class collection {
   div.className = 'book-wraper';
   div.innerHTML = `<h3>${data.title}</h3>
                   <h3>${data.author}</h3>
-                  <button type="button" class ="remove-button">Remove</button>`;
+                  <button data-value="${data.title}-${data.author}" type="button" class ="remove-button">Remove</button>`;
   bookSection.appendChild(div);
 }
+   
+   removeFromColl(data){
+     const arr = data.getAttribute('data-value').split('-');
+     this.books = this.books.filter(item => item.title !== arr[0] && item.author !== arr[1]);
+   }
+
 }
 
 class book {
@@ -39,5 +53,7 @@ const coll = new collection;
 submitBtn.addEventListener('click', () => {
   coll.add(new book(inputTitle.value, inputAuthor.value));
 })
+
+
 
 
