a5a8d8e05cf233d235bd31d07cb9f5429e6a156e
lisandroseia <lisandroseia11@gmail.com>
2022-01-04 16:30:18 -0300

    Change data saving to classes

--- script.js
+++ script.js
@@ -1,56 +1,34 @@
-let collection = [];
-let arr = [];
-
 const inputTitle = document.getElementById('title');
 const inputAuthor = document.getElementById('author');
 const submitBtn = document.querySelector('.add-btn');
 const bookSection = document.querySelector('.books');
 
-const book = {
-  title: '',
-  author: '',
-};
+class collection {
+  constructor(books = []) {
+    this.books = books;
+  }
 
-function populateStorage() {
-  localStorage.setItem('Books', JSON.stringify({
-    coll: collection,
-  }));
+  add(data){
+     this.books.push(data);
+  }
+
+  remove(data){
+    
+  }
+
 }
 
-function addBook(book) {
-  const div = document.createElement('div');
-  div.className = 'book-wraper';
-  div.setAttribute('data-value', `${book.title}-${book.author}`);
-  div.innerHTML = `<h3>${book.title}</h3>
-                  <h3>${book.author}</h3>
-                  <button type="button" class ="remove-button">Remove</button>`;
-  bookSection.appendChild(div);
-  const removeBtn = document.querySelectorAll('.remove-button');
-  removeBtn[removeBtn.length - 1].addEventListener('click', (evt) => {
-    document.querySelector('.books').removeChild(evt.target.parentNode);
-    arr = evt.target.parentNode.getAttribute('data-value').split('-');
-    collection = collection.filter((item) => item.title !== arr[0] && item.author !== arr[1]);
-    populateStorage();
-  });
+class book {
+ constructor(title, author){
+   this.title = title;
+   this.author = author;
+ }
 }
 
-submitBtn.addEventListener('click', () => {
-  if (inputTitle.value !== '' && inputAuthor.value !== '') {
-    const newBook = Object.create(book);
-    newBook.title = inputTitle.value;
-    newBook.author = inputAuthor.value;
-    collection.push(newBook);
-    populateStorage();
-    addBook(newBook);
-  }
-});
+const coll = new collection;
 
-window.onload = () => {
-  if (localStorage.getItem('Books')) {
-    const localBooks = JSON.parse(localStorage.getItem('Books'));
-    collection = localBooks.coll;
-    localBooks.coll.forEach((element) => {
-      addBook(element);
-    });
-  }
-};
+submitBtn.addEventListener('click', () =>{
+coll.add(  new book(inputTitle.value, inputAuthor.value));
+})
+
+
