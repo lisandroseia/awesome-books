c76e06c6a51eec6973f0b9392b999737c12dd34f
Zeeshan Haider <zhadier39@gmail.com>
2022-01-05 19:47:13 +0500

    styling

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ class Collection {
     if (this) {
       const div = document.createElement('div');
       div.className = 'book-wraper';
-      div.innerHTML = `<h3>${data.title}</h3>
+      div.innerHTML = `<h3>"${data.title}" by</h3>
                     <h3>${data.author}</h3>
                     <button data-value="${data.title}-${data.author}" type="button" class ="remove-button">Remove</button>`;
       bookSection.appendChild(div);
